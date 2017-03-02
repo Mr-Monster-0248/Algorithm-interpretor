16346a8ff55376b99f41b38be9376ee4b1f83d7e
Thibault <tibolepezfr@gmail.com>
2017-03-02 20:57:22 +0100

    broke project

--- set.bat
+++ set.bat
@@ -3,7 +3,7 @@ doskey make=gcc sources/*.c -o ExeTest -Wall
 doskey make_release=gcc sources/*.c -o Algo_Interpreter -Wall
 doskey make_noWall=gcc sources/*.c -o ExeTest_NoWall
 doskey ls=dir
-doskey clear=cls
+#doskey clear=cls
 doskey bite=echo C TRO RIGODROL LOL
 doskey noam=echo is a faggot
 echo Console set!
