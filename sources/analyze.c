549192d887cd878e37b5273fa04756d80f59017e
Thibault <tibolepezfr@gmail.com>
2017-03-03 12:51:28 +0100

    2nd attempt solving the UNIX bug

--- sources/analyze.c
+++ sources/analyze.c
@@ -103,7 +103,7 @@ int get_line_elements(const char* line, char*** elements, int** types, int* posi
 	//Erasing the previous arrays (if there were previous arrays)
 	if (*types != NULL)
 	{
-		for (i = 0; i < (*types)[0]; i++)
+		for (i = 0; i < (*types)[0]-1; i++)
 			free((*elements)[i]);
 		free(*elements);
 
