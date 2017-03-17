73dc1b27de979ce0b80744ce6a43c83884b12587
Camboy <camille.briand.20160281@efrei.net>
2017-03-17 09:03:42 +0100

    Function of shifting implemented

--- headers/load.h
+++ headers/load.h
@@ -25,6 +25,9 @@
 	void free_2D_char_array(char*** array, int subs);
 
 	//Function that shifts all the value of an integer array to the left from a given subscript (which will be overwritten in the operation)
-	void shift_left(int** array, int* arraySize, int shiftFrom);
+	void shift_left__int_array(int** array, int* arraySize, int shiftFrom);
+
+	//Function that shifts all the value of an integer array to the left from a given subscript (which will be overwritten in the operation)
+	void shift_left__string_array (char*** array, int* arraySize, int shiftFrom);
 
 #endif
